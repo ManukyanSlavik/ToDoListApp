b018e59240b2a7a2d279ecdd46cae94ae1b2b673
ManukyanSlavik <SlavikManukyan@hotmail.com>
2025-05-26 12:58:02 +0200

    Added drag and drop functionality. Some bugs are present, require fixing.

--- Script.js
+++ Script.js
@@ -2,6 +2,7 @@ let input;
 let list;
 let finishedTaskList;
 let date;
+let id = 0;
 
 window.onload = function(){
     input = document.getElementById("taskName");
@@ -11,6 +12,7 @@ window.onload = function(){
     date = new Date();
     const currDate = "Today is " + date.toLocaleDateString();
     document.getElementById("date").textContent = currDate;
+    loadData();
 };
 
 function addTask(){
@@ -23,11 +25,16 @@ function addTask(){
         li.innerHTML = input.value;
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        li.setAttribute("draggable", true);
+        li.setAttribute("ondragstart", "onDragStart(event)")
+        li.setAttribute("id", id);
+        id++;
 
         list.appendChild(li);
     }
 
     input.value = '';
+    saveData();
 }
 
 function toggleTheme() {
@@ -47,4 +54,36 @@ document.addEventListener("click", function(e){
     }
     else if(e.target.tagName === "SPAN")
         e.target.parentElement.remove();
+
+    saveData();
 });
+
+function saveData(){
+    localStorage.setItem("toDos", list.innerHTML);
+    localStorage.setItem("finished", finishedTaskList.innerHTML);
+}
+function loadData(){
+    list.innerHTML = localStorage.getItem("toDos");
+    finishedTaskList.innerHTML = localStorage.getItem("finished");
+}
+
+function onDragStart(e){
+    e.dataTransfer.setData("id", e.target.id);
+}
+function dragOverHandler(e){
+    e.preventDefault();
+}
+function dropHandler(e){
+    e.preventDefault(); 
+    // e.target.getElementsByTagName('ul')[0].appendChild(document.getElementById(e.dataTransfer.getData("id")));
+    const elemId = e.dataTransfer.getData("id");
+    const elem = document.getElementById(elemId);
+
+    elem.classList.toggle("checked");
+    console.log(e.target);
+
+    if(elem.classList.contains("checked"))
+        finishedTaskList.appendChild(elem);
+    else
+        list.appendChild(elem);
+}
