d00b210132d193d1895f673d8b3415f57b16250c
ManukyanSlavik <SlavikManukyan@hotmail.com>
2025-05-27 14:11:36 +0200

    Small change

---
+++ LocalStorageManager.js
@@ -0,0 +1,42 @@
+function saveData(){
+    let data = [...app.list.children]
+                .filter(el => el.tagName === "LI" && el.getAttribute("class") !== "bottomElem")
+                .map(li => {
+                    return li.textContent.slice(0, -1).trim(); // slice() is to remove the <span> at the end.
+                });
+
+    localStorage.setItem("toDos", data);
+
+    data = [...app.finishedTaskList.children]
+            .filter(el => el.tagName === "LI" && el.getAttribute("class") !== "bottomElem")
+            .map(li => {
+                return li.textContent.slice(0, -1).trim(); // slice() is to remove the <span> at the end.
+            });
+
+    localStorage.setItem("finished", data);
+}
+function loadData(){
+    let data = localStorage.getItem("toDos");
+    if (data !== null && data !== ''){
+        data = data.split(','); 
+    
+        for (let i = 0; i < data.length; i++){
+            let li = buildTask(data[i]);
+    
+            app.list.prepend(li);
+        }
+    }
+
+    data = localStorage.getItem("finished");
+    if (data !== null && data !== ''){
+        data = data.split(',');
+    
+        for (let i = 0; i < data.length; i++){
+            let li = buildTask(data[i]);
+
+            li.classList.toggle("checked");
+    
+            app.finishedTaskList.prepend(li);
+        }
+    }
+}
